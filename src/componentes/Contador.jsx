d44dfd579fb15f0cf644e452ac971a19470c969d
tomacho <tomacho28.th@gmail.com>
2024-09-12 10:58:06 -0300

    agregamos log para restar 1 al estado

--- src/componentes/Contador.jsx
+++ src/componentes/Contador.jsx
@@ -1,31 +1,29 @@
-
 import { useState } from "react";
 
 const Contador = () => {
+  // mayor parte de la logica
 
-    // mayor parte de la logica 
+  // let numero = 1
 
-    // let numero = 1
+  // crear un estado
 
-    // crear un estado 
+  const [numero, setNumero] = useState(1);
 
-     const [numero,setNumero] = useState(1) 
+  const sumar = () => {
+    //numero++
+    setNumero(numero + 1);
+  };
 
-    const sumar = () => {
-        //numero++ 
-        setNumero(numero + 1)
-        console.log(numero)
-    }
- 
   return (
-
-  <section className="text-center my-5">
-    <h2>Contador</h2>
-    <h4>{numero}</h4>
-    <button className="btn btn-primary me-3" onClick={sumar}>+1</button>
-    <button className="btn btn-danger">-1</button>
-  </section>)
-  
+    <section className="text-center my-5">
+      <h2>Contador</h2>
+      <h4>{numero}</h4>
+      <button className="btn btn-primary me-3" onClick={sumar}>
+        +1
+      </button>
+      <button className="btn btn-danger" onClick={() => setNumero(numero - 1) }>-1</button>
+    </section>
+  );
 };
 
-export default Contador; 
+export default Contador;
